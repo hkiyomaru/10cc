ef2291d02b40d8bf4552564bea7c9baf1653164d
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2020-05-06 12:28:16 +0900

    implement addition and subtraction

--- 9cc.c
+++ 9cc.c
@@ -2,14 +2,32 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Invalid argument");
+    if(argc != 2) {
+        fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
         return 1;
     }
+
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+    while(*p) {
+        if(*p == '+') {
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        if(*p == '-') {
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        fprintf(stderr, "Invalid character: '%c'\n", *p);
+        return 1;
+    }
+
     printf("  ret\n");
     return 0;
 }
--- test.sh
+++ test.sh
@@ -17,5 +17,6 @@ assert() {
 
 assert 0 0
 assert 42 42
+assert 42 21+63-42
 
 echo OK
