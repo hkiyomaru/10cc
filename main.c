b2e9d6e0a88431c2965a1777b2b24ee028c5315e
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2020-09-03 13:31:30 +0900

    document main.c

--- main.c
+++ main.c
@@ -3,29 +3,20 @@
 void usage() { error("Usage: ./9cc <program>"); }
 
 int main(int argc, char **argv) {
-    // Show help when there is no argument.
     if (argc == 1) {
         usage();
     }
 
-    // Make user input acceessible.
     user_input = argv[1];
 
-    // Tokenize user input.
     Vector *tokens = tokenize();
-
-    // Construct abstract syntax trees.
     Program *prog = parse(tokens);
-
-    // Apply semantic parsing to the constructed ASTs.
     sema(prog);
 
 #if LOGLEVEL <= 2
-    // Draw the abstract syntax tree.
     draw_ast(prog);
 #endif
 
-    // Generate code.
     gen_x86(prog);
 
     return 0;
